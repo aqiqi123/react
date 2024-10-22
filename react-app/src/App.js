a4e73963e6f86c94cbda6dcda6db50ae79251c98
aqiqi123 <huangqisi7624@qq.com>
2024-10-22 17:37:36 +0800

    第二天，明天记得带耳机

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -21,6 +21,10 @@ const list=[
     },
 ]
 
+//高阶函数
+const isSearched=searchTerm=>item=>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 //可以直接改该文件来改变网站
 class App extends Component {
 
@@ -33,8 +37,26 @@ class App extends Component {
     //属性名与变量名相同时，可以直接list，
     this.state = {
       list: list,
-      helloWorld: 'Hello',
+      searchTerm: '',
     };
+
+    //bind方法用于绑定this指针，否则this指针会指向window
+    //这个称作类的绑定，可以让方法内的this指向当前实例对象
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(event){
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+
+    //更新state,必须使用setState方法，不可以直接修改state
+    this.setState({list: updatedList});
   }
 
   //render()方法返回一个jsx元素，该元素同时包含html和JavaScript代码
@@ -42,22 +64,37 @@ class App extends Component {
     //声明变量关键字const和let更常见
     return (
       <div className="App">
-        <h1>{this.state.helloWorld}</h1>
 
-        {this.state.list.map((item)=>{
-          return (
-            //key是虚拟DOM的唯一标识符，在重新渲染时React会使用它来确定哪些节点需要更新
-            //新旧虚拟DOM的key相同时，若内容有变化，React会更新节点内容，否则不会更新
-            //若key不一致，React会认为是不同的节点，会创建新的节点，并重新渲染
-            <div key={item.objectID}>
-              <span><a href='{item.url}'>{item.title}</a></span>
-              <span> by {item.author}</span>
-              <span> {item.num_comments} comments</span>
-              <span> {item.points} points</span>
-            </div>
-          );
-        })}
-        
+        <form>
+          <input type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item=>
+          {
+            return (
+              //key是虚拟DOM的唯一标识符，在重新渲染时React会使用它来确定哪些节点需要更新
+              //新旧虚拟DOM的key相同时，若内容有变化，React会更新节点内容，否则不会更新
+              //若key不一致，React会认为是不同的节点，会创建新的节点，并重新渲染
+              <div key={item.objectID}>
+                <span><a href='{item.url}'>{item.title}</a></span>
+                <span> by {item.author}</span>
+                <span> {item.num_comments} comments</span>
+                <span> {item.points} points</span>
+  
+                <span>
+                  <button 
+                    onClick={()=>this.onDismiss(item.objectID)}
+                    type='button'
+                  >
+                    Dismiss
+                  </button>
+                </span>
+              </div>
+            );
+          }
+        )}
+
       </div>
     );
   }
