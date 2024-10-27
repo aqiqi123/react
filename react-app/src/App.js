a0d0fcad982193f1b80c6555b77c02b7f63a4485
aqiqi123 <huangqisi7624@qq.com>
2024-10-27 14:32:37 +0800

    Update App.js
    学了fetch api，但是有点难懂啊

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from'react';
 import './App.css';
 
-//定义一个数组，包含一些示例数据，反映的是我们准备用API获取的数据
-const list=[
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'John',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-    },
-]
+//下面是API相关的变量和函数
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE='https://hn.algolia.com/api/v1';
+const PATH_SEARCH='search';
+const PARAM_SEARCH='query=';
+
+//组件生命周期顺序
+//constructor（）
+//componentWillMount（）
+//render（）
+//componentDidMount（）
+
+//组件更新生命周期
+//componentWillReceiveProps（）
+//shouldComponentUpdate（）
+//componentWillUpdate（）
+//render（）
+//componentDidUpdate（）
+
+//组件卸载生命周期
+//componentWillUnmount（）
 
 //高阶函数
 const isSearched=searchTerm=>item=>
@@ -36,14 +39,34 @@ class App extends Component {
     //state用于组件保存·控制·修改自己的可变状态 
     //属性名与变量名相同时，可以直接list，
     this.state = {
-      list: list,
-      searchTerm: '',
+      result:null,
+      searchTerm: DEFAULT_QUERY,
     };
 
     //bind方法用于绑定this指针，否则this指针会指向window
     //这个称作类的绑定，可以让方法内的this指向当前实例对象
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result});
+  }
+
+  fetchSearchTopStories(searchTerm) {
+    const url = `${PATH_BASE}/${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`;
+    fetch(url)
+     .then(response => response.json())
+     .then(result => this.setSearchTopStories(result))
+     .catch(error => console.error(error));
+  }
+
+  //组件挂载后，调用fetchSearchTopStories方法，获取初始数据
+  componentDidMount() {
+    const {searchTerm} = this.state;
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onSearchChange(event){
@@ -62,7 +85,9 @@ class App extends Component {
   //render()方法返回一个jsx元素，该元素同时包含html和JavaScript代码
   render() {
     //解构，让代码更加简洁
-    const {list, searchTerm} = this.state;
+    const {searchTerm,result} = this.state;
+
+    if (!result) {return null;}
 
     //声明变量关键字const和let更常见
     //当我们想要在自己创建的组件中再嵌套其他元素，就会用到children属性
@@ -78,7 +103,7 @@ class App extends Component {
         </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
